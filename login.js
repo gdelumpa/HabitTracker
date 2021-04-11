2cb3f591c490fbe7e0de44ae37699b434b9afb10
Gillian Delumpa <44386181+gdelumpa@users.noreply.github.com>
2021-04-11 15:51:32 -0500

    Update login.js (#3)
    completed simple login validation, need redirection from successful login to either: 1. existing habits or 2. new habits

--- login.js
+++ login.js
@@ -1,19 +1,35 @@
 var objPeople = [
 {
-    username: "sam",
-    password: "codify"
+    username: "user",
+    password: "login"
+},
+{
+    username: "gdelumpa",
+    password: "pass"
 }
 ]
 
 function getInfo(){
-    var username = document.getElementById("username").nodeValue
-    var password = document.getElementById("password").nodeValue
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+    var togglePopup = document.querySelector(".popup");
+    var x = 0;
 
-    for(i =0; i< objPeople.length; i++){
-        if(username == objPeople[i].username && password ==objPeople[i].password){
-            console.log(username + "is logged in!")
-            return
+    for(i = 0; i< objPeople.length; i++){
+        if(username == objPeople[i].username && password == objPeople[i].password){
+            // check if there's any habits in system
+            // existing habits > go to index.html
+
+            // non-existing habits > go to createhabit.html
+            window.location.assign("createhabit.html")
+            return false;
+            x = 1;
         }
     }
-    console.log("incorrect username or password")
-}
+    if(x == 0){ 
+        togglePopup.style.display = "flex";
+        document.querySelector(".close").addEventListener("click", function(){
+            document.querySelector(".popup").style.display = "none";
+        })
+}}
+
